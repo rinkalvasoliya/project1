ec26216593f9d3374646275ba320fa8c5355fd9d
Rinkal Vasoliya <Rinkal.Vasoliya@teknopoint.local>
2023-04-11 19:39:30 +0530

    first project

---
+++ name/script.js
@@ -0,0 +1,19 @@
+
+$(document).on('ready', function () {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+});
